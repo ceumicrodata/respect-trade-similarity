ca10749a784c55ac42cd5e095145f6b8e4769b15
gaborberei <berei.gabor1@gmail.com>
2019-06-14 13:47:09 +0200

    BUG FIXED: export/import are coded in  #10

--- code/1_exp_imp_multi.py
+++ code/1_exp_imp_multi.py
@@ -14,13 +14,13 @@ def exp_imp(data_name):
 
     ########################################################
     data = pd.read_csv(source_in+data_name)
-    data = data[data.PRODUCT_NC!="TOTAL"]
+    data = data[data.PRODUCT_NC!="TOTAL"][data.STAT_REGIME == 1]
     data["PRODUCT_NC"] = data["PRODUCT_NC"].apply(lambda x: str(x[:4]))
-    sixdigit_product_trade = pd.DataFrame(data.groupby(["TRADE_TYPE",'DECLARANT_ISO','PARTNER_ISO',"PRODUCT_NC"])['VALUE_IN_EUROS'].sum().reset_index())
+    sixdigit_product_trade = pd.DataFrame(data.groupby(["FLOW",'DECLARANT_ISO','PARTNER_ISO',"PRODUCT_NC"])['VALUE_IN_EUROS'].sum().reset_index())
     ########################################################
 
-    data_EXP = sixdigit_product_trade[sixdigit_product_trade["TRADE_TYPE"]=="E"].drop("TRADE_TYPE",axis=1)
-    data_IMP = sixdigit_product_trade[sixdigit_product_trade["TRADE_TYPE"]=="I"].drop("TRADE_TYPE",axis=1)
+    data_EXP = sixdigit_product_trade[sixdigit_product_trade["FLOW"]==2].drop("TRADE_TYPE",axis=1)
+    data_IMP = sixdigit_product_trade[sixdigit_product_trade["FLOW"]==1].drop("TRADE_TYPE",axis=1)
 
     ########################################################
     data_EXP.to_csv(source_out + '/exp_imp/'+ "EXP_" +data_name)
